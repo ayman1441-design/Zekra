88c5434bb377e9eb0aff3fad050b0b04d732352b
ayman1441-design <ayman1441@gmail.com>
2026-01-28 22:59:50 +0300

    Update sub.js

--- sub.js
+++ sub.js
@@ -5,16 +5,16 @@ $(`<center>
         <u><b><font color="red">الإشتراكات والفعالياات</font></b></u>
       </div>
       <div id="spanan" style="font-family: jazeera-light, 'Cairo', sans-serif;color: #0d3d5c;text-align: center;display:none;">
-        <p style="text-align: center;margin: 2px 0;"><b><font color="#ad0000">نتمنّى لكم صنع ذكريات سعيدة في شات ذكرى الخليج، الفعاليات الحالية : روم المسابقات (مسابقات يومية على مدار الساعة) ، روم الفعاليات (فعاليات متنوعة يومية) الحائط (كت تويت وإبداع متواصل) (</font></b></p>
+        <p style="text-align: center;margin: 2px 0;"><b><font color="#ad0000">نتمنّى لكم صنع ذكريات سعيدة في شات ذكرى الخليج، الفعاليات الحالية : روم المسابقات (مسابقات يومية على مدار الساعة) ، روم الفعاليات (فعاليات متنوعة يومية) الحائط (كت تويت وإبداع متواصل)</font></b></p>
         <p style="text-align: center;margin: 2px 0;"><b>أنواع الإشتراكات:</b></p>
-        <p style="text-align: center;margin: 2px 0;"><b><font color="#1e5a8e">سوبر اشتراك</font> - <font color="red">500 ريال شهرياً</font></b></p>
-        <p style="text-align: center;margin: 2px 0;"><b><font color="#1e5a8e">سوبر اشتراك كامل</font> - <font color="red">750 ريال شهرياً</font></b></p>
+        <p style="text-align: center;margin: 2px 0;"><b><font color="#1e5a8e">سوبر اشتراك</font> - <font color="red">250 ريال شهرياً</font></b></p>
+        <p style="text-align: center;margin: 2px 0;"><b><font color="#1e5a8e">سوبر اشتراك كامل</font> - <font color="red">350 ريال شهرياً</font></b></p>
         <p style="text-align: center;margin: 2px 0;">
           <a href="https://zekra.net/esh.html" target="_blank" onclick="event.stopPropagation();" style="color: #1e5a8e; font-weight: bold; text-decoration: underline;">صفحة الإشتراكات</a> | 
           <a href="https://zekra.net/rules.html" target="_blank" onclick="event.stopPropagation();" style="color: #1e5a8e; font-weight: bold; text-decoration: underline;">قوانين الشات</a> | 
           <a href="https://zekra.net/decoration.html" target="_blank" onclick="event.stopPropagation();" style="color: #1e5a8e; font-weight: bold; text-decoration: underline;">زخرفة الإسم</a>
         </p>
-        <p style="text-align: center;margin: 2px 0 0 0;padding-bottom: 5px;"><u><b><font color="red">الإدارة | وريد</font></b></u></p>
+        <p style="text-align: center;margin: 2px 0 0 0;padding-bottom: 5px;"><u><b><font color="red"><3</font></b></u></p>
       </div>
     </div>
   </div>
